39060692ec9a6abbecbf05d782ee1baf5b71c1bd
“Sourav <elsesourav@gmail.com>
2026-08-29 14:21:47 +0530

    fix(database): remove earlyAccess property from prisma.config.ts

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -7,7 +7,6 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env.local') });
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join(__dirname, 'prisma', 'schema.prisma'),
   datasource: {
     url: process.env.DATABASE_URL || '',
