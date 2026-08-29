cd87651f8ead9768cb3d357075103bbcfe1fd1b5
“Sourav <elsesourav@gmail.com>
2026-08-29 14:21:22 +0530

    fix(database): update datasource type in prisma.config.ts

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -11,6 +11,5 @@ export default defineConfig({
   schema: path.join(__dirname, 'prisma', 'schema.prisma'),
   datasource: {
     url: process.env.DATABASE_URL || '',
-    directUrl: process.env.DIRECT_URL || '',
   },
 });
